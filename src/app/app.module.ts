baf7c6b37d177fdabadbebf754bd5a5e0f7fd47b
Jose Toledo <jotatoledon@gmail.com>
2017-05-29 20:45:19 +0200

    refactored(app module): basic config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared';
+
+import { AppRoutingModule, routedComponents } from './app-routing.module';
+
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routedComponents
   ],
   imports: [
+    SharedModule,
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    Title
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
