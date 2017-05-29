a9e19b551ebe79fc71a2ff5b6003abcc00258260
Jose Toledo <jotatoledon@gmail.com>
2017-05-29 23:40:10 +0200

    feat(app component): added iconography
    * Registered custom icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MdIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'ws-root',
@@ -8,8 +10,18 @@ import { Title } from '@angular/platform-browser';
 })
 export class AppComponent {
   constructor(
+    private _iconRegistry: MdIconRegistry,
+    private _domSanitizer: DomSanitizer,
     private _title: Title
   ) {
+    this.registerIcons();
     this._title.setTitle('Angular Workshop -  Book Store');
+  }
+
+  private registerIcons() {
+    this._iconRegistry.addSvgIconInNamespace('icons', 'github',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg'));
+    this._iconRegistry.addSvgIconInNamespace('icons', 'bookshelf',
+      this._domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/bookshelf.svg'));
   }
 }
