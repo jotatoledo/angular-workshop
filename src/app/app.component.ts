faca8fea4e5f7899112cbeceb367f277be636271
Jose Toledo <jotatoledon@gmail.com>
2017-05-29 20:43:38 +0200

    refactored(app component): changed to minor config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'ws-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ws works!';
+  constructor(
+    private _title: Title
+  ) {
+    this._title.setTitle('Angular Workshop -  Book Store');
+  }
 }
