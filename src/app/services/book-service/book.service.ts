4e13600fa028a1a7b34859a67200999c135d2ec4
Jose Toledo <jotatoledon@gmail.com>
2017-05-30 21:50:46 +0200

    feat(book service): added local collection
    * Added collection of books and methods to manage it

--- src/app/services/book-service/book.service.ts
+++ src/app/services/book-service/book.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, RequestOptions, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
 import { extractData, handleError } from '../util';
-import { BookQuery, BookDetail } from '../../models';
+import { BookQuery, BookDetail, BookPresentation } from '../../models';
 
 /**
  * API specification: https://developers.google.com/books/docs/v1/using
@@ -10,9 +11,11 @@ import { BookQuery, BookDetail } from '../../models';
 @Injectable()
 export class BookService {
   private _endPoint: string;
+  private readonly collection: BookPresentation[];
   constructor(
     private _http: Http
   ) {
+    this.collection = [];
     this._endPoint = 'https://www.googleapis.com/books/v1/volumes';
   }
 
@@ -38,5 +41,32 @@ export class BookService {
       .map(extractData)
       .catch(handleError)
       .delay(300);
+  }
+
+  addToCollection(book: BookDetail) {
+    const index = this.collection.findIndex(b => b.id === book.id);
+    if (index === -1) {
+      const newElement: BookPresentation = {
+        id: book.id,
+        volumeInfo: book.volumeInfo
+      };
+      this.collection.push(newElement);
+    }
+  }
+
+  removeFromCollection(id: string) {
+    const index = this.collection.findIndex(b => b.id === id);
+    if (index !== -1) {
+      this.collection.splice(index, 1);
+    }
+  }
+
+  isInCollection(id: string): Observable<boolean> {
+    const inCollection = this.collection.find(b => b.id === id);
+    return of(inCollection !== undefined);
+  }
+
+  getCollection(): Observable<BookPresentation[]> {
+    return of(this.collection);
   }
 }
