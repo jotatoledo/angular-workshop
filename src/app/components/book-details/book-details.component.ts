7477e8132e817b765c2ee3e6a48c99f953c9de70
Jose Toledo <jotatoledon@gmail.com>
2017-05-30 19:00:06 +0200

    feat(): added book details component

---
+++ src/app/components/book-details/book-details.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { BookDetail } from '../../models';
+
+@Component({
+  selector: 'ws-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent {
+  @Input() book: BookDetail;
+  @Input() inCollection: boolean;
+  @Output() add = new EventEmitter<string>();
+  @Output() remove = new EventEmitter<string>();
+
+  handleAdd() {
+    this.add.emit(this.id);
+  }
+
+  handleRemove() {
+    this.remove.emit(this.id);
+  }
+
+  /**
+    * Tip: Utilize getters to keep templates clean
+    */
+  get id() {
+    return this.book.id;
+  }
+
+  get title() {
+    return this.book.volumeInfo.title;
+  }
+
+  get subtitle() {
+    return this.book.volumeInfo.subtitle;
+  }
+
+  get description() {
+    return this.book.volumeInfo.description;
+  }
+
+  get thumbnail() {
+    return this.book.volumeInfo.imageLinks
+      && this.book.volumeInfo.imageLinks.smallThumbnail;
+  }
+}
