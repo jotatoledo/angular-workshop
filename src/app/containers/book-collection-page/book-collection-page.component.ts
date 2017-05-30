87bc6806d04768f391e3c6571cf250b476dc4146
Jose Toledo <jotatoledon@gmail.com>
2017-05-30 21:06:35 +0200

    feat(): added book collection component

---
+++ src/app/containers/book-collection-page/book-collection-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { BookService } from '../../services';
+import { BookPresentation } from '../../models';
+
+@Component({
+  selector: 'ws-book-collection-page',
+  templateUrl: './book-collection-page.component.html',
+  styleUrls: ['./book-collection-page.component.css']
+})
+export class BookCollectionPageComponent {
+  books$: Observable<BookPresentation[]>;
+  constructor(
+    private _bookService: BookService
+  ) {
+    this.books$ = Observable.of([]);
+  }
+}
