1ee46a6d792d2c64b1f0df4850ea8b1f65b7b83a
JotaToledo <fixed-term.JoseIgnacio.ToledoNavarro@de.bosch.com>
2017-06-09 15:12:01 +0200

    feature(): added collection module

---
+++ src/app/books/collection/collection-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BookDetailsPageComponent, NotFoundPageComponent } from 'app/shared';
+
+import { BookCollectionPageComponent } from './containers';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookCollectionPageComponent
+  },
+  {
+    path: ':id',
+    component: BookDetailsPageComponent
+  },
+  {
+    path: '404',
+    component: NotFoundPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CollectionRoutingModule { }
+export const routedComponents = [
+  BookCollectionPageComponent
+];
