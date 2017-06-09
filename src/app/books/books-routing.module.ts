77f62b8bc446d47ef36467535e7b838d1d9fcf33
JotaToledo <fixed-term.JoseIgnacio.ToledoNavarro@de.bosch.com>
2017-06-09 15:12:40 +0200

    feature(): added books module

---
+++ src/app/books/books-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {
+  LayoutPageComponent
+} from './containers';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'collection',
+        pathMatch: 'full'
+      },
+      {
+        path: 'store',
+        loadChildren: './store/store.module#StoreModule'
+      },
+      {
+        path: 'collection',
+        loadChildren: './collection/collection.module#CollectionModule'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class BooksRoutingModule { }
+
+export const routedComponents = [
+  LayoutPageComponent
+];
