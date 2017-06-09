8ed63d9406ef3f81ed7b411691fbe6bf09ccf2a7
JotaToledo <fixed-term.JoseIgnacio.ToledoNavarro@de.bosch.com>
2017-06-09 15:12:01 +0200

    feature(): added collection module

---
+++ src/app/books/collection/collection.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedModule } from 'app/shared';
+
+import { CollectionRoutingModule, routedComponents } from './collection-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SharedModule,
+    CollectionRoutingModule
+  ],
+  declarations: [
+    routedComponents
+  ]
+})
+export class CollectionModule { }
