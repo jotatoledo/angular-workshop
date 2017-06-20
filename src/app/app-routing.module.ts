2e7f0396cc900f955444d1520c49beb033f9673f
Jose Toledo <jotatoledon@gmai.com>
2017-06-20 21:35:48 +0200

    feature(): added book guard
    refactored(app routing): added layout component to 404 route
    
    feature(store/collection modules): added route guard
    
    * Added a route guard to first check existence of the requested book
    
    feat(): added entry points

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { NotFoundPageComponent } from './shared';
+import { NotFoundPageComponent, LayoutPageComponent } from './shared';
 
 const routes: Routes = [
     { path: '', loadChildren: 'app/landing/landing.module#LandingModule', pathMatch: 'full' },
     { path: 'books', loadChildren: 'app/books/books.module#BooksModule' },
-    { path: '404', component: NotFoundPageComponent },
+    { path: '404', component: LayoutPageComponent, children: [{ path: '', component: NotFoundPageComponent }] },
     { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
 
