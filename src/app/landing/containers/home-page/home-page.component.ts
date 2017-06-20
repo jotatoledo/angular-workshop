5fd85b0bb189951fe46aff6ef9d9dd43f486f80a
Jose Toledo <jotatoledon@gmail.com>
2017-06-20 17:03:30 +0200

    feature(): added home page component

---
+++ src/app/landing/containers/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'ws-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomePageComponent {
+
+}
