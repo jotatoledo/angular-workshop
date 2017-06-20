dbb48e89622414ceb3568dc5030bfa33697745b1
Jose Toledo <jotatoledon@gmail.com>
2017-06-20 15:14:31 +0200

    refactored(): moved modules out of books module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { NotFoundPageComponent } from './shared';
 
 const routes: Routes = [
     { path: '', redirectTo: 'books', pathMatch: 'full' },
-    { path: 'books', loadChildren: './books/books.module#BooksModule' },
+    { path: 'books', loadChildren: 'app/books/books.module#BooksModule' },
     { path: '404', component: NotFoundPageComponent },
     { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
