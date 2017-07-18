974f452d2b18b5c9bf3802d9ed2da535b56daf39
Jose Toledo <jotatoledon@gmail.com>
2017-07-18 12:52:36 +0200

    fixed(): change detection on store components

--- src/app/books/store/components/book-search/book-search.component.ts
+++ src/app/books/store/components/book-search/book-search.component.ts
@@ -4,8 +4,7 @@ import { TdSearchBoxComponent } from '@covalent/core';
 @Component({
   selector: 'ws-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./book-search.component.css']
 })
 export class BookSearchComponent {
   @ViewChild(TdSearchBoxComponent)
@@ -17,6 +16,7 @@ export class BookSearchComponent {
   set filter(value: string) {
     this._filter = value;
     this.box.value = value;
+    // TODO fix setting this component change detection to onPush will result in the wrong display of the input field
   }
 
 
