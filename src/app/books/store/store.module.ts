72438f93ee07d1c821862a99864f60bddfb37fbb
Jose Toledo <jotatoledon@gmail.com>
2017-07-18 12:39:43 +0200

    feature(store module): added effects

--- src/app/books/store/store.module.ts
+++ src/app/books/store/store.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SharedModule } from 'app/shared';
-
-import {  BookSearchComponent} from './components';
-import { StoreRoutingModule, routedComponents } from './store-routing.module';
 import { CovalentLoadingModule } from '@covalent/core';
+import { EffectsModule } from '@ngrx/effects';
+import 'rxjs/add/operator/finally';
+
+import { StoreEffects } from './ngrx/effects';
+import { SharedModule } from 'app/shared';
+import { BookSearchComponent } from './components';
+import { StoreRoutingModule, routedComponents } from './store-routing.module';
 
 @NgModule({
   imports: [
@@ -13,6 +15,7 @@ import { CovalentLoadingModule } from '@covalent/core';
     SharedModule,
     CovalentLoadingModule,
     StoreRoutingModule,
+    EffectsModule.forFeature([StoreEffects])
   ],
   declarations: [
     BookSearchComponent,
