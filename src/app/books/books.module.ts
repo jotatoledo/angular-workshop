b4285efa2b6f192e38818b4b89d21bae466f208a
Jose Toledo <jotatoledon@gmail.com>
2017-07-18 12:39:54 +0200

    feature(books module): added reducer/state to store

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'app/shared';
+import { StoreModule } from '@ngrx/store';
 
+import { reducers } from './ngrx/reducer';
 import { BooksRoutingModule, routedComponents } from './books-routing.module';
 import { BookService } from './services';
 import { BookGuard } from './guards';
@@ -9,6 +11,7 @@ import { BookGuard } from './guards';
 @NgModule({
   imports: [
     CommonModule,
+    StoreModule.forFeature('books', reducers),
     SharedModule,
     BooksRoutingModule
   ],
