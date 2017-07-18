c3d2d2dc86d0bb006882b9100e8b81c661a9d054
Jose Toledo <jotatoledon@gmail.com>
2017-07-18 12:37:29 +0200

    refactored(book search component): added input property
    * Added input of filter value

--- src/app/books/store/components/book-search/book-search.component.ts
+++ src/app/books/store/components/book-search/book-search.component.ts
@@ -1,12 +1,27 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, ChangeDetectionStrategy, Input, ViewChild } from '@angular/core';
+import { TdSearchBoxComponent } from '@covalent/core';
 
 @Component({
   selector: 'ws-book-search',
   templateUrl: './book-search.component.html',
-  styleUrls: ['./book-search.component.css']
+  styleUrls: ['./book-search.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookSearchComponent {
-  @Output() search = new EventEmitter<string>();
+  @ViewChild(TdSearchBoxComponent)
+  box: TdSearchBoxComponent;
+
+  private _filter: string;
+  @Input('filter')
+  get filter() { return this._filter; }
+  set filter(value: string) {
+    this._filter = value;
+    this.box.value = value;
+  }
+
+
+  @Output()
+  search = new EventEmitter<string>();
 
 
   handleSearch(event: string) {
