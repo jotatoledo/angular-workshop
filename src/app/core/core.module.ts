d3fe8a5b7b548ab06d4c60ae29901f090126636f
Jose Toledo <jotatoledon@gmail.com>
2018-12-28 22:40:43 +0100

    refactor(core): follow up changes
    * Provide core services
    * Export landing view

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,26 +1,34 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule, MatButtonModule } from '@angular/material';
 import { CovalentLoadingModule } from '@covalent/core';
 
 import { throwIfAlreadyLoaded } from './module-import.guard';
 import { HomePageComponent } from './views/home-page/home-page.component';
+import {
+  BookService,
+  NavigationService,
+  BookCollectionService
+} from './services';
+import { BookGuard } from './guards';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    FlexLayoutModule,
     CovalentLoadingModule
   ],
-  declarations: [
-    HomePageComponent
-  ],
-  exports: [
-    HomePageComponent
-  ]
+  declarations: [HomePageComponent],
+  providers: [NavigationService, BookService, BookCollectionService, BookGuard],
+  exports: [HomePageComponent]
 })
 export class CoreModule {
-  constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
   }
 }
--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -1,3 +1,4 @@
-export * from './services/navigation.service';
+export * from './services';
+export * from './guards';
 export * from './views/home-page/home-page.component';
 export * from './core.module';
